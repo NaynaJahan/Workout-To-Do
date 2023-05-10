bbc4d493f2d35d9df1f8fcf5e9cfa97754375351
Nayna Jahan Neha <naynajahanneha@Naynas-MacBook-Pro.local>
2023-05-10 20:54:32 +0600

    Initial commit

---
+++ backend/routes/workouts.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const {
+    getWorkouts,
+    getWorkout,
+    createWorkout,
+    deleteWorkout,
+    updateWorkout,
+} = require('../controllers/workoutController')
+
+const requireAuth = require('../middleware/requireAuth')
+
+const router = express.Router()
+
+// require auth for all workout routes
+router.use(requireAuth)
+
+//get all workouts
+router.get('/', getWorkouts)
+
+//get a single workout
+router.get('/:id',getWorkout)
+
+//Post a new workout
+router.post('/', createWorkout)
+
+//Delete a workout
+router.delete('/:id', deleteWorkout)
+
+//update a workout
+router.patch('/:id', updateWorkout)
+
+module.exports = router
+
+
